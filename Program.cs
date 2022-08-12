d29b0a67fa31827879baa893729a7f9ad239bb14
Ankitsehrawat01 <sehrawata328@gmail.com>
2022-08-12 18:47:03 +0530

    Program to calculate Max String

--- Program.cs
+++ Program.cs
@@ -6,3 +6,7 @@ Console.WriteLine();
 //for float
 float value2 = maximum.MaximumFloat(12.3f, 1.3f, 3.6f);
 Console.WriteLine("Max float value is : " + value2);
+Console.WriteLine();
+//for string
+string name = maximum.MaximumStringNumber("An", "Ank", "Ankit");
+Console.WriteLine("Max string is : " + name);
--- maximum.cs
+++ maximum.cs
@@ -66,5 +66,34 @@ namespace Max_values_using_Generics
             }
             throw new Exception("num4,num5 and num6 are same");
         }
+        public static string MaximumStringNumber(string firstString, string secondString, string thirdString)
+        {
+
+            if (firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) > 0 ||
+
+                firstString.CompareTo(secondString) >= 0 && firstString.CompareTo(thirdString) > 0 ||
+
+                firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) >= 0)
+            {
+                return firstString;
+            }
+            if (secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) > 0 ||
+
+                secondString.CompareTo(firstString) >= 0 && secondString.CompareTo(thirdString) > 0 ||
+
+                secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) >= 0)
+            {
+                return secondString;
+            }
+            if (thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) > 0 ||
+
+                thirdString.CompareTo(firstString) >= 0 && thirdString.CompareTo(secondString) > 0 ||
+
+                thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) >= 0)
+            {
+                return thirdString;
+            }
+            throw new Exception("firstNumber,secondNumber and thirdNumber are same");
+        }
     }
 }
